e2a94159233c2da9dc502d10a8e5e0e011b64b1a
Sofyan Sugianto <sofyanartem@gmail.com>
2018-04-30 15:20:38 +0700

    fix on pressed and exiting area2d keys

--- Area2D.gd
+++ Area2D.gd
@@ -2,17 +2,35 @@ extends Area2D
 
 var a = 0
 var tekan = false
+var on_note = false
 onready var test = get_parent().get_node('Label')
 
-func _on_Area2D_area_entered(area): 
+func _ready():
+	set_process(true)
+
+func _process(delta):
+	if tekan:
+		a+=10
+		test.set_text(str(int(a)))
+		pass
+	pass
+
+
+func _on_Area2D_area_entered(area):
 	if area.get_name()=="note1":
-		if tekan == true:
-			a+=10
-			test.set_text(str(int(a)))
-	print (tekan)
+		on_note = true
+		set_process(true)
 
 func _input(ev):
 	if ev is InputEventKey:
 		if ev.pressed and ev.scancode == KEY_A:
-#			ev.is_action_pressed()
 			tekan = true
+			print(tekan)
+		if not ev.pressed and ev.scancode == KEY_A:
+			tekan = false
+			print(tekan)
+
+func _on_Area2D_area_exited(area):
+	set_process(false)
+	on_note = false
+	tekan = false
